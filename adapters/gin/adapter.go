3d83ae37d052b4eafef4b89ab3afeef4168416c5
robusgauli <robusgauli@gmail.com>
2020-01-30 12:35:26 +0545

    Add adapter for gin middleware

---
+++ adapters/gin/adapter.go
@@ -0,0 +1,61 @@
+package adapter
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/opentracing/opentracing-go"
+	"github.com/opentracing/opentracing-go/ext"
+)
+
+const (
+	// ContextKey is a key for context value of span
+	ContextKey string = "opencensus-span"
+)
+
+// Trace defines the middleware for gin that wraps around the handler
+// and creates span
+func Trace() gin.HandlerFunc {
+
+	return func(c *gin.Context) {
+
+		tracer := opentracing.GlobalTracer()
+		wireContext, err := tracer.Extract(opentracing.HTTPHeaders, opentracing.HTTPHeadersCarrier(c.Request.Header))
+		var span opentracing.Span
+
+		if err != nil {
+			span = tracer.StartSpan(
+				c.HandlerName(),
+			)
+		} else {
+			span = tracer.StartSpan(
+				c.HandlerName(),
+				ext.RPCServerOption(wireContext),
+			)
+		}
+
+		span.SetTag(string(ext.SpanKind), string(ext.SpanKindRPCClientEnum))
+		span.SetTag(string(ext.PeerHostname), c.Request.Host)
+		span.SetTag(string(ext.HTTPUrl), c.Request.URL.Path)
+		span.SetTag(string(ext.HTTPMethod), c.Request.Method)
+
+		// Wrap the request with span context
+		c.Request = wrapRequestWithSpanContext(c.Request, span)
+		c.Set(ContextKey, span)
+		// call the next
+		c.Next()
+		// set the response headers
+		tracer.Inject(span.Context(), opentracing.HTTPHeaders, opentracing.HTTPHeadersCarrier(c.Writer.Header()))
+		span.SetTag(string(ext.HTTPStatusCode), c.Writer.Status())
+
+		span.Finish()
+		fmt.Println("finished ", span.Context())
+	}
+}
+
+func wrapRequestWithSpanContext(req *http.Request, span opentracing.Span) *http.Request {
+	ctx := opentracing.ContextWithSpan(req.Context(), span)
+	req = req.WithContext(ctx)
+	return req
+}
